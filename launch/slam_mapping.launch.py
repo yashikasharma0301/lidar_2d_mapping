88c6fe23b77933fe106ed4ccc9dca87ffadea796
Yashika Sharma <yashikasharma0301@gmail.com>
2025-08-13 12:18:21 +0530

    Update slam_mapping.launch.py

--- launch/slam_mapping.launch.py
+++ launch/slam_mapping.launch.py
@@ -6,20 +6,17 @@ from ament_index_python.packages import get_package_share_directory
 from launch_ros.actions import Node
 
 def generate_launch_description():
-    # Package paths
+
     slam_pkg = get_package_share_directory('slam_toolbox')
     this_pkg = get_package_share_directory('lidar-2d-mapping')
 
-    # Your local mapping params
     mapping_params = os.path.join(this_pkg, 'params', 'mapper_params_online_async.yaml')
 
-    # Cyclone DDS for performance
     set_rmw = SetEnvironmentVariable(
         name='RMW_IMPLEMENTATION',
         value='rmw_cyclonedds_cpp'
     )
-
-    # Include official SLAM Toolbox mapping launch with your params
+    
     slam_toolbox_launch = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
             os.path.join(slam_pkg, 'launch', 'online_async_launch.py')
@@ -30,7 +27,6 @@ def generate_launch_description():
         }.items()
     )
 
-    # RViz with sim time
     rviz_config_path = os.path.join(this_pkg, 'rviz', 'slam_view.rviz')
     rviz_node = Node(
         package='rviz2',
